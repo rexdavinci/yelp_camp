7fd70463fbae4c7f7cbfcbb5d844413b61d9b46e
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2019-12-14 20:45:29 +0700

    space to welcome message

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.post('/register', function (req, res) {
             return res.render('register', {error: err.message});
         }
         passport.authenticate('local')(req, res, function () {
-            req.flash("success", "Welcome to Yelpcamp" + user.username);
+            req.flash("success", "Welcome to Yelpcamp " + user.username);
             res.redirect('/campgrounds');
         });
     });
